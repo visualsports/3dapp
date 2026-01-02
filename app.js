f928019827ac3d59103cd16e04744ac812cb17f5
visualsports <junglaultimate@gmail.com>
2026-01-02 17:05:37 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('*', (req, res) => {
@@ -10,5 +11,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log('Visualynx Server Active');
+    console.log('Visualynx Server running...');
 });
