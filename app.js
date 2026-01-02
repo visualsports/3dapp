06f3dedac3760b4a3c057d21a1898d4687c40026
visualsports <junglaultimate@gmail.com>
2026-01-02 17:42:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos
+// Le dice a Express que use la carpeta 'public' como raíz
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Cualquier ruta que no sea un archivo, entrega el index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
-    console.log('Visualynx Server running...');
+    console.log('Visualynx Server Ready');
 });
