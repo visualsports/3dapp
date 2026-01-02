3a3fc9c91f0f270c1a7f175a385533ddbd5e26f8
visualsports <junglaultimate@gmail.com>
2026-01-02 16:28:13 -0500

    app.js

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Servir archivos estáticos desde la carpeta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Ruta principal para el editor
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, '0.0.0.0', () => {
-  console.log('Editor de Visual Sport listo');
+    console.log('Visualynx Server Active');
 });
